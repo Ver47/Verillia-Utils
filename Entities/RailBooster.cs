8f24ba4538844cd42dff735dee2a52f959575786
Verillia <154223944+Ver47@users.noreply.github.com>
2024-01-02 00:14:13 +0800

    Added empty RailBooster class...

---
+++ Entities/RailBooster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Celeste.Mod;
+using Celeste;
+using Monocle;
+using Celeste.Mod.Entities;
+
+namespace Celeste.Mod.Verillia.Utils {
+    [CustomEntity()]
+    public class RailBooster
+    {
+    }
+}
